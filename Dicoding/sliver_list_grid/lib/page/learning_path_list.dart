e253bae40d7edb23c46ba2d51ce426ac998818ed
Frengky <frengky.gg@gmail.com>
2022-05-10 10:28:17 +0700

    finished

---
+++ Dicoding/sliver_list_grid/lib/page/learning_path_list.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:sliver_list_grid/models/academy.dart';
+
+class LearningPathPage extends StatelessWidget {
+  const LearningPathPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dicoding Learning Paths'),
+      ),
+      body: LearningPathList(),
+    );
+  }
+}
+
+class LearningPathList extends StatelessWidget {
+  const LearningPathList({Key? key}) : super(key: key);
+  SliverPersistentHeader _header(String text) {
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: SliverAppBarDelegate(
+        minHeight: 60,
+        maxHeight: 150,
+        child: Container(
+          color: Colors.lightBlue,
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        _header('Android Developer'),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            androidPaths.map(_buildTile).toList(),
+          ),
+        ),
+        _header('iOS Developer'),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            iosPaths.map(_buildTile).toList(),
+          ),
+        ),
+        _header('Multi-Platform App Developer'),
+        SliverGrid.count(
+          crossAxisCount: 2,
+          children: flutterPaths.map(_buildTile).toList(),
+        ),
+        _header('Front-End Web Developer'),
+        SliverGrid.count(
+          crossAxisCount: 2,
+          children: webPaths.map(_buildTile).toList(),
+        )
+      ],
+    );
+  }
+}
+
+class SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  SliverAppBarDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return child;
+  }
+
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  bool shouldRebuild(covariant SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.maxHeight ||
+        child != oldDelegate.child;
+  }
+}
+
+Widget _buildTile(Academy academy) {
+  return ListTile(
+    title: Text(academy.title),
+    subtitle: Text(academy.description),
+  );
+}
