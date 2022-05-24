58ff41c5a72b2951f2c6bf7aa007452d14a24e01
Frengky <frengky.gg@gmail.com>
2022-05-24 10:57:41 +0700

    add apiservice, list and detail models

---
+++ Dicoding/restaurant_app/lib/data/api/api_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:restaurant_app/data/models/restaurant_detail.dart';
+import 'package:restaurant_app/data/models/restaurant_list.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String _baseUrl = 'https://restaurant-api.dicoding.dev/';
+
+  Future<RestaurantList> list() async {
+    final response = await http.get(Uri.parse(_baseUrl + "list"));
+    if (response.statusCode == 200) {
+      return RestaurantList.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load Restaurant List');
+    }
+  }
+
+  Future<RestaurantDetail> detail(int id) async {
+    final response = await http.get(Uri.parse(_baseUrl + "detail/$id"));
+    if (response.statusCode == 200) {
+      return RestaurantDetail.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load Restaurant Detail');
+    }
+  }
+}
