ce0d2448b5fc75806b6f5b6b7ace032c5bc2a361
Frengky <frengky.gg@gmail.com>
2022-06-06 22:20:17 +0700

    add transaction models & data

--- Flutter The Complete Guide/02 Personal Expense/my_expense_manager/lib/main.dart
+++ Flutter The Complete Guide/02 Personal Expense/my_expense_manager/lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:my_expense_manager/transaction.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Weekly Groceries',
+      amount: 16.34,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
