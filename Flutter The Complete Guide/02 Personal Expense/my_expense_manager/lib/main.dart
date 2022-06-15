f09946764b068c27e65b7b5d618930a9df58f7c9
Frengky <frengky.gg@gmail.com>
2022-06-15 11:42:32 +0700

    show modal bottom sheet when click add

--- Flutter The Complete Guide/02 Personal Expense/my_expense_manager/lib/main.dart
+++ Flutter The Complete Guide/02 Personal Expense/my_expense_manager/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:my_expense_manager/models/transaction.dart';
-import 'package:my_expense_manager/widgets/user_transaction.dart';
+import 'package:my_expense_manager/widgets/new_transaction.dart';
+import 'package:my_expense_manager/widgets/transaction_list.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,11 +13,64 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyAppOne(),
+    );
+  }
+}
+
+class MyAppOne extends StatefulWidget {
+  const MyAppOne({Key? key}) : super(key: key);
+
+  @override
+  State<MyAppOne> createState() => _MyAppOneState();
+}
+
+class _MyAppOneState extends State<MyAppOne> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.34,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return NewTransaction(addTx: _addNewTransaction);
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('AppBar'), actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => _startAddNewTransaction(context),
             icon: Icon(Icons.add),
           )
         ]),
@@ -33,13 +87,13 @@ class MyApp extends StatelessWidget {
                   elevation: 5,
                 ),
               ),
-              UserTransaction(),
+              TransactionList(transactions: _userTransactions),
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {},
+          onPressed: () => _startAddNewTransaction(context),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
