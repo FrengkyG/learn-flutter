49da5a36a602712822f076ed01434798fad3c92f
Frengky <frengky.gg@gmail.com>
2022-07-26 15:27:29 +0700

    add filter product fav or not

--- Flutter The Complete Guide/04 Shop App/shop_app/lib/screens/products_overview_screen.dart
+++ Flutter The Complete Guide/04 Shop App/shop_app/lib/screens/products_overview_screen.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/widgets/product_item.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products_provider.dart';
 
-import '../models/product.dart';
 import '../widgets/product_grid.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductOverviewScreen extends StatefulWidget {
   ProductOverviewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProductOverviewScreen> createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  var _showOnlyFavorite = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('MyShop')),
-      body: ProductsGrid(),
+      appBar: AppBar(
+        title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+              onSelected: (FilterOptions selectedValue) {
+                setState(() {
+                  if (selectedValue == FilterOptions.Favorites) {
+                    _showOnlyFavorite = true;
+                  } else {
+                    _showOnlyFavorite = false;
+                  }
+                });
+              },
+              icon: Icon(Icons.more_vert),
+              itemBuilder: (_) => [
+                    PopupMenuItem(
+                      child: Text('Only Favorites'),
+                      value: FilterOptions.Favorites,
+                    ),
+                    PopupMenuItem(
+                      child: Text('Show All'),
+                      value: FilterOptions.All,
+                    ),
+                  ])
+        ],
+      ),
+      body: ProductsGrid(showFavs: _showOnlyFavorite),
     );
   }
 }
