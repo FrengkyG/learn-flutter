8c57075617353fcfec51e088814cba004ac91b14
Frengky <frengky.gg@gmail.com>
2022-08-10 16:08:14 +0700

    change isfavorite also change on firebase

--- Flutter The Complete Guide/04 Shop App/shop_app/lib/models/product.dart
+++ Flutter The Complete Guide/04 Shop App/shop_app/lib/models/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,27 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  void toggleFavoriteStatus() async {
+    final url = Uri.parse(
+        'https://flutter-udemy-9b856-default-rtdb.asia-southeast1.firebasedatabase.app/products/$id.json');
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    try {
+      final response = await http.patch(url,
+          body: json.encode({
+            'isFavorite': isFavorite,
+          }));
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
