8af90d4c878f851f8d33ddc242fb142c0a9f15f9
Frengky <frengky.gg@gmail.com>
2022-08-11 10:34:31 +0700

    add login method & catch auth error

--- Flutter The Complete Guide/04 Shop App/shop_app/lib/providers/auth_provider.dart
+++ Flutter The Complete Guide/04 Shop App/shop_app/lib/providers/auth_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_exception.dart';
 import 'package:shop_app/private.dart';
 
 class Auth with ChangeNotifier {
@@ -9,20 +10,35 @@ class Auth with ChangeNotifier {
   DateTime? _expiryDate;
   String? _userId;
 
-  Future<void> signup(String username, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
-            apiKey);
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': username,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$apiKey');
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> signup(String username, String password) async {
+    return _authenticate(username, password, "signUp");
+  }
+
+  Future<void> login(String username, String password) async {
+    return _authenticate(username, password, "signInWithPassword");
   }
 }
