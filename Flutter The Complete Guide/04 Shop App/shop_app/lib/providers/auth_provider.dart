26d224af814b348c727a63c49225a51742c69177
Frengky <frengky.gg@gmail.com>
2022-08-12 21:36:17 +0700

    add: auto logout function

--- Flutter The Complete Guide/04 Shop App/shop_app/lib/providers/auth_provider.dart
+++ Flutter The Complete Guide/04 Shop App/shop_app/lib/providers/auth_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
@@ -9,6 +10,7 @@ class Auth with ChangeNotifier {
   String? _token;
   DateTime? _expiryDate;
   String? _userId;
+  Timer? _authTimer;
 
   bool get isAuth {
     return token != '';
@@ -51,6 +53,7 @@ class Auth with ChangeNotifier {
       _expiryDate = DateTime.now().add(
         Duration(seconds: int.parse(responseData['expiresIn'])),
       );
+      _autoLogout();
       notifyListeners();
     } catch (error) {
       throw error;
@@ -69,6 +72,18 @@ class Auth with ChangeNotifier {
     _token = '';
     _userId = '';
     _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer!.cancel();
+      _authTimer = null;
+    }
     notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_authTimer != null) {
+      _authTimer!.cancel();
+    }
+    final timeToExpiry = _expiryDate!.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
