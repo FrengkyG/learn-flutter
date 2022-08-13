e048afe87ccc221cc4c2b45cc2e42fcee3a7f194
Frengky <frengky.gg@gmail.com>
2022-08-13 10:19:24 +0700

    change: animation use AnimatedContainer

--- Flutter The Complete Guide/04 Shop App/shop_app/lib/screens/auth_screen.dart
+++ Flutter The Complete Guide/04 Shop App/shop_app/lib/screens/auth_screen.dart
@@ -212,12 +212,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation!.value.height,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation!.value.height,
         constraints: BoxConstraints(minHeight: _heightAnimation!.value.height),
         width: deviceSize.width * 0.75,
         padding: const EdgeInsets.all(16.0),
+
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
