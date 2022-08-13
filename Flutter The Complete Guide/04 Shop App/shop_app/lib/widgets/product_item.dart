533e1a97a44ad245e7bbb919b3e9503ae9d1daaf
Frengky <frengky.gg@gmail.com>
2022-08-13 13:29:33 +0700

    add: hero animation

--- Flutter The Complete Guide/04 Shop App/shop_app/lib/widgets/product_item.dart
+++ Flutter The Complete Guide/04 Shop App/shop_app/lib/widgets/product_item.dart
@@ -24,12 +24,15 @@ class ProductItem extends StatelessWidget {
               Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
                   arguments: product.id);
             },
-            child: FadeInImage(
-                placeholder:
-                    const AssetImage('assets/images/product-placeholder.png'),
-                image: NetworkImage(
-                  product.imageUrl,
-                ))),
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                  placeholder:
+                      const AssetImage('assets/images/product-placeholder.png'),
+                  image: NetworkImage(
+                    product.imageUrl,
+                  )),
+            )),
         footer: GridTileBar(
           backgroundColor: Colors.black38,
           leading: Consumer<Product>(
