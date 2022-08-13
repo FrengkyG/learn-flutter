2d69e0cf1570a98208acb5747755645651fc39dc
Frengky <frengky.gg@gmail.com>
2022-08-13 16:03:47 +0700

    add: ChangeNotifierProvider & home route

--- Flutter The Complete Guide/05 Great Places App/great_places_app/lib/main.dart
+++ Flutter The Complete Guide/05 Great Places App/great_places_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places_app/providers/great_places_provider.dart';
+import 'package:great_places_app/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider(
+      create: (_) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          // ignore: deprecated_member_use
+          accentColor: Colors.amber,
+        ),
+        home: const PlacesListScreen(),
       ),
-      home: Scaffold(),
     );
   }
 }
