0d4c973dd19b01562c4b18f9d46fb7123f2fc06d
Frengky <frengky.gg@gmail.com>
2022-08-13 13:58:00 +0700

    add: custom transition page

--- Flutter The Complete Guide/04 Shop App/shop_app/lib/main.dart
+++ Flutter The Complete Guide/04 Shop App/shop_app/lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/helpers/custom_route.dart';
 import 'package:shop_app/providers/auth_provider.dart';
 import 'package:shop_app/providers/cart_provider.dart';
 import 'package:shop_app/providers/orders_provider.dart';
@@ -49,6 +50,10 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.purple,
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
+            pageTransitionsTheme: PageTransitionsTheme(builders: {
+              TargetPlatform.android: CustomPageTransitionBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionBuilder(),
+            }),
           ),
           home: auth.isAuth
               ? const ProductOverviewScreen()
