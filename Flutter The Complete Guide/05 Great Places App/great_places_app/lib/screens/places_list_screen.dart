9d538eba5e67f9d49080dc05e15901e6702ba347
Frengky <frengky.gg@gmail.com>
2022-08-13 21:20:56 +0700

    add: show data

--- Flutter The Complete Guide/05 Great Places App/great_places_app/lib/screens/places_list_screen.dart
+++ Flutter The Complete Guide/05 Great Places App/great_places_app/lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places_app/providers/great_places_provider.dart';
 import 'package:great_places_app/screens/add_place_screen.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({Key? key}) : super(key: key);
@@ -18,9 +20,23 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
-      ),
+      body: Consumer<GreatPlaces>(
+          child: const Center(
+              child: Text('Got no places yet, start adding some!')),
+          builder: (ctx, greatPlaces, ch) => greatPlaces.items.isEmpty
+              ? ch!
+              : ListView.builder(
+                  itemCount: greatPlaces.items.length,
+                  itemBuilder: (ctx, i) => ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: FileImage(
+                        greatPlaces.items[i].image,
+                      ),
+                    ),
+                    title: Text(greatPlaces.items[i].title),
+                    onTap: () {},
+                  ),
+                )),
     );
   }
 }
