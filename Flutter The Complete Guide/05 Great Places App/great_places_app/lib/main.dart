686ecc159368d188586fd2408d2383a298796a3d
Frengky <frengky.gg@gmail.com>
2022-08-13 15:46:02 +0700

    initial commit

---
+++ Flutter The Complete Guide/05 Great Places App/great_places_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Great Places',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.amber,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
