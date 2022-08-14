1c175f83187def3f9153ad097b32aaa33165513e
Frengky <frengky.gg@gmail.com>
2022-08-14 20:25:20 +0700

    add: get current user location method

--- Flutter The Complete Guide/05 Great Places App/great_places_app/lib/widgets/location_input.dart
+++ Flutter The Complete Guide/05 Great Places App/great_places_app/lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({Key? key}) : super(key: key);
@@ -9,6 +10,10 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String? _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +39,7 @@ class _LocationInputState extends State<LocationInput> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentUserLocation,
               icon: const Icon(Icons.location_on),
               label: Text(
                 'Current Location',
