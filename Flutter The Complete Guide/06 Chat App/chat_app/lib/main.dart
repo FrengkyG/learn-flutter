cff4c37c0bddbf5001640dc42c3beb98a33e2db3
Frengky <frengky.gg@gmail.com>
2022-08-16 22:09:33 +0700

    finishing auth redirect & logout

--- Flutter The Complete Guide/06 Chat App/chat_app/lib/main.dart
+++ Flutter The Complete Guide/06 Chat App/chat_app/lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:chat_app/screens/auth_screen.dart';
 import 'package:chat_app/screens/chat_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -18,30 +19,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterChat',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        backgroundColor: Colors.pink,
-        accentColor: Colors.deepPurple,
-        accentColorBrightness: Brightness.dark,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.pink,
-            onPrimary: Colors.white,
-            fixedSize: const Size(140, 50),
-            padding: const EdgeInsets.all(10),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
+        title: 'FlutterChat',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          backgroundColor: Colors.pink,
+          accentColor: Colors.deepPurple,
+          accentColorBrightness: Brightness.dark,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.pink,
+              onPrimary: Colors.white,
+              fixedSize: const Size(140, 50),
+              padding: const EdgeInsets.all(10),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              primary: Colors.pink,
             ),
           ),
         ),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            primary: Colors.pink,
-          ),
-        ),
-      ),
-      home: const AuthScreen(),
-    );
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (ctx, userSnapshot) {
+              if (userSnapshot.hasData) {
+                return const ChatScreen();
+              }
+              return const AuthScreen();
+            }));
   }
 }
--- Flutter The Complete Guide/06 Chat App/chat_app/lib/screens/chat_screen.dart
+++ Flutter The Complete Guide/06 Chat App/chat_app/lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,32 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterChat'),
+        actions: [
+          DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                    child: Row(
+                      children: const [
+                        Icon(Icons.exit_to_app),
+                        SizedBox(width: 8),
+                        Text('Logout'),
+                      ],
+                    ),
+                    value: 'logout'),
+              ],
+              onChanged: (itemIdentifier) {
+                if (itemIdentifier == 'logout') {
+                  FirebaseAuth.instance.signOut();
+                }
+              })
+        ],
+      ),
       body: StreamBuilder(
           stream: FirebaseFirestore.instance
               .collection('chats/kmb32kaA11TuJU3bwv3h/messages')
